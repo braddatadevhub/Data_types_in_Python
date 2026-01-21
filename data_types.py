a6792b922983b3a5a38ec54294ed9acfde757ffe
Bradley <braddatadevhub@gmail.com>
2026-01-21 03:02:08 +0300

    Add data_types.py

---
+++ data_types.py
@@ -0,0 +1,35 @@
+#strings ,str
+"Hello, I am Bradley from braddatadevhub and I am subscribed to this channel"
+#integers, int
+56, 6878, 8789
+#float
+7.8
+#boolean, bool
+#True False
+#binary
+'bytarrays', 'bytes'
+
+greetings = "Hello, I am from braddatadevhub and I am subscribed to this channel"
+
+numbers =  56
+
+gpa = 7.8
+
+subscription = True
+
+print(type(subscription))
+
+
+#simple
+20+40
+
+greeting = "Hello"
+#compound
+# a,b=b,a
+
+# for i in greeting:
+#     print(i)
+
+    
+print("Hello\
+      World")
